6c3aa08bb87e194e410eece4147be4055590bc14
Leam Howe <leamhowe1@gmail.com>
2026-09-09 15:44:12 +0100

    add alternative baselayers to munro map and remove stadia

--- munro_map/generate_map.py
+++ munro_map/generate_map.py
@@ -31,10 +31,14 @@ m = leafmap.Map(center=[56.8, -4.2], zoom=7)
 
 m.add_basemap("OpenTopoMap", name="OpenTopoMap")
 # m.add_basemap("Esri.WorldTopoMap", name="Esri Topo", show=False)
-# m.add_basemap("Esri.WorldImagery", name="Satellite", show=False)
-m.add_basemap("SATELLITE", name="Satellite", show=False)
-m.add_basemap("Stadia.StamenWatercolor", name="Watercolor", show=False)
+m.add_basemap("Esri.WorldImagery", name="Satellite", show=False)
+# m.add_basemap("SATELLITE", name="Satellite", show=False)
+# m.add_basemap("Stadia.StamenWatercolor", name="Watercolor", show=False)
+m.add_basemap("Esri.NatGeoWorldMap", name="National Geographic", show=False)
+m.add_basemap("WaymarkedTrails.hiking", name="Hiking Routes", show=False)
+m.add_basemap("OpenSnowMap.pistes", name="Snow & Ski Routes", show=False)
 m.add_basemap("Strava.All", name="Strava", show=False)
+
 
 # 4. Add the Munros to the map
 # We loop through the data to add them so we can apply the custom colors perfectly
